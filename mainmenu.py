5d2780a3093272e234bf2e9b075b5df9d30e74d8
Terebo <63092966+Terebo@users.noreply.github.com>
2020-05-22 16:00:12 +0200

    yay, still not finished

---
+++ mainmenu.py
@@ -0,0 +1,24 @@
+import sys
+import colorama
+def mainmenu():
+    print("choose:\n \033[1;33;40msingleplayer\n \033[1;30;40mmultiplayer\n \033[1;31;40mexit\33[0m")
+    inputd = input("->")
+    eval(inputd + "()")
+
+def exit():
+    sys.exit()
+
+def multiplayer():
+    print("nope not yet!")
+    mainmenu()
+
+def singleplayer():
+    print("choose:\n \033[1;32;40measy \n \033[1;31;40mnormal\33[0m")
+    inputd = input("-->")
+    if(inputd == "easy"):
+        import singleplayer
+    if(inputd == "normal"):
+        import singleplayerH
+
+colorama.init()
+mainmenu()
